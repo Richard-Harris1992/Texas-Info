49b1565a22daae882b84ae5df39aa806043dde30
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2023-03-07 14:12:58 -0600

    finished styling, and sources

--- src/Views/Sources.js
+++ src/Views/Sources.js
@@ -14,7 +14,7 @@ const Sources = () => {
         <li>"History of Texas." Wikipedia, The Free Encyclopedia. Wikimedia Foundation, Inc., 28 Feb. 2023. Web. 06 Mar. 2023. <a href="https://en.wikipedia.org/wiki/History_of_Texas" className="sourceLink">https://en.wikipedia.org/wiki/History_of_Texas</a>.</li>
         <li>"Texas Tourist Attractions." Tour Texas. Tour Texas, 2023. Web. 06 Mar. 2023. <a href="https://www.tourtexas.com/attractions" className="sourceLink">https://www.tourtexas.com/attractions</a>.</li>
         <li>"Visit Austin, Texas | Explore Hotels, Music, Restaurants, Attractions and Things to Do." Visit Austin, 2023, <a href="https://www.austintexas.org/" className="sourceLink">https://www.austintexas.org/</a>.</li>
-        <li>"VisitDallas: Dallas TX Attractions & Things To Do." VisitDallas, 2023, <a href="https://www.visitdallas.com/" className="sourceLink">https://www.visitdallas.com/</a>.</li>
+        <li>"Visit Dallas: Dallas TX Attractions & Things To Do." VisitDallas, 2023, <a href="https://www.visitdallas.com/" className="sourceLink">https://www.visitdallas.com/</a>.</li>
         <li>"Visit Houston Texas | Explore Hotels, Restaurants, Events & Things to Do." Visit Houston, 2023, <a href="https://www.visithoustontexas.com/" className="sourceLink">https://www.visithoustontexas.com/</a>.</li>
         <li>"Visit San Antonio | Official Tourism Site." Visit San Antonio, 2023, <a href="https://www.visitsanantonio.com/" className="sourceLink">https://www.visitsanantonio.com/</a>.</li>
       </ol>
